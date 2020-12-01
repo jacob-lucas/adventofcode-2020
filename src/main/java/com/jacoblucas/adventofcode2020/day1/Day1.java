fed964d5053d9547a35f22534213f18eecf9b1a2
Jacob Lucas <jacob.lucas@gmail.com>
2020-11-30 21:38:09 -0800

    Day 1 part 2

--- src/main/java/com/jacoblucas/adventofcode2020/day1/Day1.java
+++ src/main/java/com/jacoblucas/adventofcode2020/day1/Day1.java
@@ -4,37 +4,61 @@ import com.google.common.collect.ImmutableSet;
 import com.jacoblucas.adventofcode2020.utils.InputReader;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day1 {
+    public static final int TOTAL = 2020;
+
     public static void main(String[] args) throws IOException {
         final Set<Integer> expenses = InputReader.read("day1-input.txt").stream()
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toSet());
 
-        final Set<Integer> entries = sumTo(2020, expenses);
-        if (entries.size() != 2) {
+        final Set<Integer> twoEntries = findTwo(TOTAL, expenses);
+        if (twoEntries.size() != 2) {
             throw new IllegalArgumentException("No expenses sum to expected result");
         }
+        System.out.println("Sum of " + twoEntries + " = " + TOTAL);
+        final int result1 = product(twoEntries);
+        System.out.println(result1);
 
-        final Iterator<Integer> iterator = entries.iterator();
-        final int one = iterator.next();
-        final int two = iterator.next();
-        final int result = one * two;
-        System.out.println(result);
+        final Set<Integer> threeEntries = findThree(TOTAL, expenses);
+        if (threeEntries.size() != 3) {
+            throw new IllegalArgumentException("No expenses sum to expected result");
+        }
+        System.out.println("Sum of " + threeEntries + " = " + TOTAL);
+        final int result2 = product(threeEntries);
+        System.out.println(result2);
     }
 
-    static Set<Integer> sumTo(final int result, final Set<Integer> input) {
+    static Set<Integer> findTwo(final int result, final Set<Integer> input) {
         for (int i : input) {
             int diff = result - i;
             if (input.contains(diff)) {
-                System.out.println(i + " + " + diff + " = " + result);
                 return ImmutableSet.of(i, diff);
             }
         }
         return ImmutableSet.of();
+    }
+
+    static Set<Integer> findThree(final int result, final Set<Integer> input) {
+        for (int i : input) {
+            int diff = result - i;
+            final Set<Integer> subInput = input.stream()
+                    .filter(n -> n != diff)
+                    .collect(Collectors.toSet());
+            final Set<Integer> others = findTwo(diff, subInput);
+            if (!others.isEmpty()) {
+                return Stream.concat(ImmutableSet.of(i).stream(), others.stream()).collect(Collectors.toSet());
+            }
+        }
+        return ImmutableSet.of();
+    }
+
+    static int product(final Set<Integer> input) {
+        return input.stream().reduce(1, (a, b) -> a * b);
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2020/day1/Day1Test.java
+++ src/test/java/com/jacoblucas/adventofcode2020/day1/Day1Test.java
@@ -11,16 +11,30 @@ import static org.junit.Assert.assertThat;
 
 public class Day1Test {
     @Test
-    public void sumToWithResult() {
+    public void findTwoWithResult() {
         final Set<Integer> input = ImmutableSet.of(1721, 979, 366, 299, 675, 1456);
-        final Set<Integer> pair = Day1.sumTo(2020, input);
+        final Set<Integer> pair = Day1.findTwo(2020, input);
         assertThat(pair, is(ImmutableSet.of(1721, 299)));
     }
 
     @Test
-    public void sumToWithoutResult() {
+    public void findTwoWithoutResult() {
         final Set<Integer> input = ImmutableSet.of(1721, 979, 366, 2999, 675, 1456);
-        final Set<Integer> pair = Day1.sumTo(2020, input);
+        final Set<Integer> pair = Day1.findTwo(2020, input);
         assertThat(pair, is(empty()));
+    }
+
+    @Test
+    public void findThreeWithResult() {
+        final Set<Integer> input = ImmutableSet.of(1721, 979, 366, 299, 675, 1456);
+        final Set<Integer> result = Day1.findThree(2020, input);
+        assertThat(result, is(ImmutableSet.of(979, 366, 675)));
+    }
+
+    @Test
+    public void findThreeWithoutResult() {
+        final Set<Integer> input = ImmutableSet.of(1721, 9799, 366, 2999, 675, 1456);
+        final Set<Integer> result = Day1.findThree(2020, input);
+        assertThat(result, is(empty()));
     }
 }
