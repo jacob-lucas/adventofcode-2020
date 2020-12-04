8701d4ae1ae260b50af9be300853225754184dc9
Jacob Lucas <jacob.lucas@gmail.com>
2020-12-04 12:02:37 -0800

    Refactor to use vavr Try objects

--- src/main/java/com/jacoblucas/adventofcode2020/day4/Day4.java
+++ src/main/java/com/jacoblucas/adventofcode2020/day4/Day4.java
@@ -1,6 +1,7 @@
 package com.jacoblucas.adventofcode2020.day4;
 
 import com.jacoblucas.adventofcode2020.utils.InputReader;
+import io.vavr.control.Try;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,15 +17,7 @@ public class Day4 {
         System.out.println(validPassports.size());
 
         final List<Passport> validValuePassports = validPassports.stream()
-                .filter(p -> {
-                    try {
-                        p.validate();
-                        return true;
-                    } catch (final Exception e) {
-                        System.out.println(p + " :: " + e.getMessage());
-                        return false;
-                    }
-                })
+                .filter(p -> Try.run(p::validate).isSuccess())
                 .collect(Collectors.toList());
 
         System.out.println(validValuePassports.size());
@@ -37,17 +30,14 @@ public class Day4 {
             final String line = input.get(i);
             if (line.isEmpty()) {
                 final List<String> inputLines = input.subList(startIdx, i);
-                try {
-                    System.out.println(inputLines);
-                    final Passport passport = Passport.parse(inputLines);
-                    validPassports.add(passport);
-                } catch (final IllegalStateException iae) {
-                    System.out.println(inputLines + " is invalid! " + iae.getMessage());
-                } catch (final Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    startIdx = i + 1;
+                System.out.println(inputLines);
+                final Try<Passport> passport = Try.of(() -> Passport.parse(inputLines));
+                if (passport.isSuccess()) {
+                    validPassports.add(passport.get());
+                } else {
+                    System.out.println(inputLines + " is invalid! " + passport.failed().get());
                 }
+                startIdx = i + 1;
             }
         }
 
