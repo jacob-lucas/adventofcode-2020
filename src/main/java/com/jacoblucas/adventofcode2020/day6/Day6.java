88c6fee69d6a625946d727bcf585b34436bf1050
Jacob Lucas <jacob.lucas@gmail.com>
2020-12-06 08:20:53 -0800

    Day 6 part 1

--- src/main/java/com/jacoblucas/adventofcode2020/day6/Day6.java
+++ src/main/java/com/jacoblucas/adventofcode2020/day6/Day6.java
@@ -1,7 +1,33 @@
 package com.jacoblucas.adventofcode2020.day6;
 
-public class Day6 {
-    public static void main(String[] args) {
+import com.jacoblucas.adventofcode2020.utils.InputReader;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Day6 {
+    public static void main(String[] args) throws IOException {
+        final List<List<String>> groups = InputReader.readGroups("day6-input.txt");
+
+        final int questionCountSum = getQuestionCountSum(groups);
+        System.out.println(questionCountSum);
+    }
+
+    public static String uniqueQuestions(final List<String> group) {
+        final Set<String> uniques = String.join("", group)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .map(String::valueOf)
+                .collect(Collectors.toSet());
+        return String.join("", uniques);
+    }
+
+    public static int getQuestionCountSum(final List<List<String>> groups) {
+        return groups.stream()
+                .map(Day6::uniqueQuestions)
+                .mapToInt(String::length)
+                .sum();
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2020/day6/Day6Test.java
+++ src/test/java/com/jacoblucas/adventofcode2020/day6/Day6Test.java
@@ -1,4 +1,77 @@
 package com.jacoblucas.adventofcode2020.day6;
 
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class Day6Test {
+    @Test
+    public void testUniqueCharsEmptyList() {
+        assertThat(Day6.uniqueQuestions(ImmutableList.of()), is(""));
+    }
+
+    @Test
+    public void testUniqueCharsEmptyString() {
+        assertThat(Day6.uniqueQuestions(ImmutableList.of("")), is(""));
+    }
+
+    @Test
+    public void example1() {
+        final List<String> questions = ImmutableList.of(
+                "abcx",
+                "abcy",
+                "abcz");
+        assertThat(Day6.uniqueQuestions(questions), is("abcxyz"));
+    }
+
+    @Test
+    public void example2() {
+        final List<String> questions = ImmutableList.of("abc");
+        assertThat(Day6.uniqueQuestions(questions), is("abc"));
+    }
+
+    @Test
+    public void example3() {
+        final List<String> questions = ImmutableList.of("a", "b", "c");
+        assertThat(Day6.uniqueQuestions(questions), is("abc"));
+    }
+
+    @Test
+    public void example4() {
+        final List<String> questions = ImmutableList.of("ab", "ac");
+        assertThat(Day6.uniqueQuestions(questions), is("abc"));
+    }
+
+    @Test
+    public void example5() {
+        final List<String> questions = ImmutableList.of("a", "a", "a", "a");
+        assertThat(Day6.uniqueQuestions(questions), is("a"));
+    }
+
+    @Test
+    public void example6() {
+        final List<String> questions = ImmutableList.of("b");
+        assertThat(Day6.uniqueQuestions(questions), is("b"));
+    }
+
+    @Test
+    public void testGetQuestionCountSumEmptyList() {
+        final List<List<String>> groups = ImmutableList.of();
+        assertThat(Day6.getQuestionCountSum(groups), is(0));
+    }
+
+    @Test
+    public void testGetQuestionCountSum() {
+        final List<List<String>> groups = ImmutableList.of(
+                ImmutableList.of("abc"),
+                ImmutableList.of("a", "b", "c"),
+                ImmutableList.of("ab", "ac"),
+                ImmutableList.of("a", "a", "a", "a"),
+                ImmutableList.of("b"));
+        assertThat(Day6.getQuestionCountSum(groups), is(11));
+    }
 }
